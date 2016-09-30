c852a4bf6e9cb1f6d9682525bc0caed197de5fa6
ankul-shippable <ankul@shippable.com>
2016-09-30 18:11:03 +0530

    install latest versions

--- test/_postgres.sh
+++ test/_postgres.sh
@@ -2,7 +2,7 @@
 # Begin service ENV variables
 export SHIPPABLE_START_POSTGRES=true;
 export SHIPPABLE_POSTGRES_PORT=5432;
-export SHIPPABLE_POSTGRES_VERSION="9.4";
+export SHIPPABLE_POSTGRES_VERSION="9.6";
 export SHIPPABLE_POSTGRES_BINARY="/usr/lib/postgresql/$SHIPPABLE_POSTGRES_VERSION/bin/postgres";
 export SHIPPABLE_POSTGRES_CMD="sudo -u postgres $SHIPPABLE_POSTGRES_BINARY -c config_file=/etc/postgresql/$SHIPPABLE_POSTGRES_VERSION/main/postgresql.conf";
 
