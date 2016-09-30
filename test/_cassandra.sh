4121379a51570561834e907328272fc1ac82dbea
deepikasl <deepika@shippable.com>
2016-09-30 15:32:01 +0530

    change test script of cassandra

--- test/_cassandra.sh
+++ test/_cassandra.sh
@@ -1,9 +1,9 @@
+#!/bin/bash -e
 # Begin service ENV variables
-export SHIPPABLE_CASSANDRA_PORT=9160;
+export SHIPPABLE_CASSANDRA_PORT=9042;
 export SHIPPABLE_CASSANDRA_BINARY="/usr/sbin/cassandra";
 export SHIPPABLE_MYSQL_CMD="$SHIPPABLE_MYSQL_BINARY -f -R";
 export SHIPPABLE_CASSANDRA_LOG="/var/log/cassandra/system.log"
-
 # End service ENV variables
 
 #
@@ -20,6 +20,10 @@ stop_service() {
   sudo su -c "kill -9 `ps aux | grep [c]assandra | awk '{print $2}'`";
 }
 
+source /u14all/test/function_start_generic.sh
+#
+# Call to start service
+#
 echo "================= Starting cassandra ==================="
 printf "\n"
 start_service
