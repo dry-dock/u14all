d90252cf4953952940f4a402f6d45685503287b7
Ragesh Krishna <ragesh@rageshkrishna.com>
2017-03-29 11:25:25 +0530

    Remove unnecessary sudo

--- version/memcached_1_4.sh
+++ version/memcached_1_4.sh
@@ -4,24 +4,24 @@ MEM_VERSION=1.4.32
 LIB_VERSION=1.0.18
 
 echo "================= Installing MemCached Prereqs ==================="
-sudo apt-get install -y libevent-dev libsasl2-dev
+apt-get install -y libevent-dev libsasl2-dev
 
 # Add memcache user to run the daemon later
-sudo groupadd -r memcache
-sudo useradd -r -g memcache memcache
+groupadd -r memcache
+useradd -r -g memcache memcache
 
 echo "================= Installing MemCached ==================="
-sudo wget http://memcached.org/files/memcached-$MEM_VERSION.tar.gz
-sudo tar xzf memcached-$MEM_VERSION.tar.gz && cd memcached-$MEM_VERSION
-sudo ./configure
-sudo make && sudo make install
-cd .. && sudo rm -rf memcached-$MEM_VERSION && sudo rm -f memcached-$MEM_VERSION.tar.gz
+wget http://memcached.org/files/memcached-$MEM_VERSION.tar.gz
+tar xzf memcached-$MEM_VERSION.tar.gz && cd memcached-$MEM_VERSION
+./configure
+make && make install
+cd .. && rm -rf memcached-$MEM_VERSION && rm -f memcached-$MEM_VERSION.tar.gz
 
 
 echo "================= Installing LibMemCached ==================="
-sudo wget https://launchpad.net/libmemcached/1.0/$LIB_VERSION/+download/libmemcached-$LIB_VERSION.tar.gz
-sudo tar xzf libmemcached-$LIB_VERSION.tar.gz && cd libmemcached-$LIB_VERSION
-sudo ./configure
-sudo make && sudo make install
-cd .. && sudo rm -rf libmemcached-$LIB_VERSION && sudo rm -f libmemcached-$LIB_VERSION.tar.gz
-sudo ldconfig
+wget https://launchpad.net/libmemcached/1.0/$LIB_VERSION/+download/libmemcached-$LIB_VERSION.tar.gz
+tar xzf libmemcached-$LIB_VERSION.tar.gz && cd libmemcached-$LIB_VERSION
+./configure
+make && make install
+cd .. && rm -rf libmemcached-$LIB_VERSION && rm -f libmemcached-$LIB_VERSION.tar.gz
+ldconfig
