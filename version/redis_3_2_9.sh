4ae3e2ad6748ebd17d9675742565342456789465
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-11 17:38:55 +0530

    update redis to 3.2 to 3.2.9

--- version/redis_3_2_9.sh
+++ version/redis_3_2_9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "================= Installing redis-server ==================="
-# Installs redis-server 3.2 from chris-lea's PPA
+
 sudo add-apt-repository ppa:chris-lea/redis-server
 sudo apt-get update
 sudo wget https://redis.io/download
