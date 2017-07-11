36eeb4d1640520dbd53ca3545ca5b9793da15aea
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-11 19:05:18 +0530

    update sqlite3 from 3 to 3.19.2

--- version/sqllite_3_19_2.sh
+++ version/sqllite_3_19_2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 echo "================= Installing SQLLite 3.19.2 ==================="
-sudo apt-get install -y sqlite3 libsqlite3-dev
+sudo add-apt-repository ppa:jonathonf/backports
+
+sudo apt-get update && sudo apt-get install sqlite3
