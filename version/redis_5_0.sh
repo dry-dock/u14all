0cdadab0b877669a727ef7fabf1ec834944ef620
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-01-04 10:43:50 +0530

    Update env

--- version/redis_5_0.sh
+++ version/redis_5_0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 echo "================= Installing redis-server ==================="
-
+export REDIS_VERSION=5:5.0.3*
 sudo add-apt-repository ppa:chris-lea/redis-server
 sudo apt-get update
 sudo wget https://redis.io/download
-sudo apt-get install -q -y redis-server=5:5.0.3*
+sudo apt-get install -q -y redis-server="$REDIS_VERSION"
